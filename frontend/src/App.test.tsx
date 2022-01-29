3d89160433c7742e4ee2139310c738a6c87dc28e
Ville Verkkonen <villeverkkonen@gmail.com>
2022-01-29 15:05:56 +0200

    Player count input tests and implementations

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,21 +1,35 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
-
-beforeEach(() => {
-  render(<App />);
-});
 
 function playerCountInput() {
   return screen.getByTestId('player-count-input');
 }
 
+beforeEach(() => {
+  render(<App />);
+});
+
 test('show page title', () => {
   expect(screen.getByText('Deepest Dungeon')).toBeInTheDocument();
 });
 
-test('has an input field for player count', () => {
+test('has a number input field for player count', () => {
   const input = playerCountInput();
   expect(input).toBeInTheDocument();
+  expect(input).toHaveAttribute('type', 'number');
   expect(input).toHaveAttribute('placeholder', 'Player count');
+});
+
+test('player count input value changes', () => {
+  const input = playerCountInput();
+  fireEvent.change(input, { target: { value: 4 } });
+  expect(input).toHaveValue(4);
+});
+
+test('player count input does not accept other than number input', () => {
+  const input = playerCountInput();
+  const originalInputValue = input.nodeValue;
+  fireEvent.change((input), { target: { value: 'text' } });
+  expect(input).toHaveValue(originalInputValue);
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form } from 'react-bootstrap';
 import './App.css';
 
 function App() {
+  const [playerCount, setPlayerCount] = useState<string>('1');
+
+  const playerCountInputChanged = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setPlayerCount(e.target.value);
+  }
+
   return (
     <div>
       <h1>Deepest Dungeon</h1>
       <div>
         <Form.Control
-          type="text"
+          type="number"
+          value={playerCount}
+          onChange={playerCountInputChanged}
           placeholder="Player count"
           data-testid="player-count-input"
         />
