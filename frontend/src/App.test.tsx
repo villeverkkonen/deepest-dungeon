3cd130bcd301a45fd5b0a30e878670cd8de35b3a
Ville Verkkonen <villeverkkonen@gmail.com>
2022-01-29 14:32:13 +0200

    Second test and implementation, react-bootstrap

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -2,7 +2,20 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders page title', () => {
-  const { getByText } = render(<App />);
-  expect(getByText('Deepest Dungeon')).toBeInTheDocument();
+beforeEach(() => {
+  render(<App />);
+});
+
+function playerCountInput() {
+  return screen.getByTestId('player-count-input');
+}
+
+test('show page title', () => {
+  expect(screen.getByText('Deepest Dungeon')).toBeInTheDocument();
+});
+
+test('has an input field for player count', () => {
+  const input = playerCountInput();
+  expect(input).toBeInTheDocument();
+  expect(input).toHaveAttribute('placeholder', 'Player count');
 });
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import { Form } from 'react-bootstrap';
 import './App.css';
 
 function App() {
   return (
     <div>
       <h1>Deepest Dungeon</h1>
+      <div>
+        <Form.Control
+          type="text"
+          placeholder="Player count"
+          data-testid="player-count-input"
+        />
+      </div>
     </div>
   );
 }
